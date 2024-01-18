60894912d9f10e4eade13ab9aa6fd464163cccee
Kenbayashi <kenbayashi.gaming.00@gmail.com>
2024-01-19 01:12:12 +0900

    feat: 2024-01-18

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,46 @@
 module Lib
-    ( someFunc
-    ) where
+  ( Parser,
+    digit',
+    add',
+    sub',
+    mul',
+    div',
+    lb,
+    rb,
+  )
+where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.Char
+
+type Parser a = String -> Maybe (a, String)
+
+is :: (Char -> Bool) -> Parser Char
+is f (x : xs) = if f x then Just (x, xs) else Nothing
+is _ [] = Nothing
+
+digit' :: Parser Char
+digit' = is isDigit
+
+add' :: Parser Char
+add' = is (== '+')
+
+sub' :: Parser Char
+sub' = is (== '-')
+
+mul' :: Parser Char
+mul' = is (== '*')
+
+div' :: Parser Char
+div' = is (== '+')
+
+lb :: Parser Char
+lb = is (== '(')
+
+rb :: Parser Char
+rb = is (== ')')
+
+rep :: Parser a -> Parser [a]
+rep p xs = case p xs of
+    Just(x', xs') -> rep p xs'
+    Nothing -> 
+rep _ [] = Nothing
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,19 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+import Lib
+import Test.HUnit
+
+main :: IO Counts
+main = do
+  runTestTT test1
+
+parseA :: Parser Char
+parseA = pChar 'a'
+
+test1 :: Test
+test1 =
+  TestList
+    [ "parseA \"\"" ~: parseA "" ~=? Nothing,
+      "parseA \"a\"" ~: parseA "a" ~=? Just ('a', ""),
+      "parseA \"abc\"" ~: parseA "abc" ~=? Just ('a', "bc"),
+      "parseA \"x\"" ~: parseA "x" ~=? Nothing,
+      "parseA \"xyz\"" ~: parseA "xyz" ~=? Nothing
+    ]
